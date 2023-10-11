9c0a09d8889f367a1e8c2e5da502ad1d613a4289
Kim Hyeonwoo <allone187@naver.com>
2023-10-11 12:16:09 +0900

    1st_setting

---
+++ myChef/src/main/java/com/thProject/myChef/MyChefApplication.java
@@ -0,0 +1,13 @@
+package com.thProject.myChef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyChefApplication.class, args);
+	}
+
+}
---
+++ myChef/src/test/java/com/thProject/myChef/MyChefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thProject.myChef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
